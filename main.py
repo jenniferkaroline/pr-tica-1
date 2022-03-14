aa89334b534be63edd46964bc1bb64af0c2697eb
Jennifer <99359081+jenniferkaroline@users.noreply.github.com>
2022-03-14 18:49:04 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import pratica
+
+pratica.questao01()
+
+
+
+
+
+pratica.questao02()
+
+
+
+
+
+
+
+pratica.questao03()
+
+
+
+
+pratica.questao04()
+
+
+
+
+pratica.questao05()
---
+++ pratica.py
@@ -0,0 +1,41 @@
+
+
+def questao01():
+  x1 = 7
+  x2 = 14
+  x3 = 21
+  print(x1 + x2 + x3)
+
+def questao02():
+  numero = int(input('Digite um numero: '))
+  
+  if (numero%2) ==0:
+    print("Par")
+  else:
+    print("Ímpar")
+
+def questao03():
+  num = int(input('me diga um numero: '))
+
+  print(num**5)
+
+def questao04():
+  idade = int(input('digite sua idade: '))
+
+  print(idade >= 18)
+
+def questao05():
+  num1 = int(input('me dê um numero: '))
+
+  num2 = int(input('me dê um segundo numero: '))
+
+  print(num1 / num2)
+  
+  
+  
+    
+    
+    
+
+  
+  
